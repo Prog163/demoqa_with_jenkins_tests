bd36a5cacdc4b5e3e35cbfc2bda3c68e0ea91d78
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-06-20 22:54:23 +0400

    Added properties tests

---
+++ src/test/java/com/zaychikov/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,21 @@
+package com.zaychikov.tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+
+    @Test
+    @Tag("properties")
+    void someTest5() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+
+        // gradle clean properties_tests -Dbrowser=opera -Dversion=93
+    }
+}
